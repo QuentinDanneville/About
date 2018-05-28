5bef72f643c30a1ae42cee0f681bbf7208fe316a
Quentin Danneville <quentin@fnfagency.fr>
2018-05-28 22:56:46 +0200

    [Front] Adding map intro animation with the 3 states

--- src/static/js/controllers/map.js
+++ src/static/js/controllers/map.js
@@ -5,6 +5,18 @@ export default function(module) {
 	function MapController($scope, MapFactory) {
 		var vm = this;
 
+		vm.showMapState = true;
+
 		vm.CF = MapFactory;
+
+		setTimeout(()=> {
+			$scope.$apply(()=> {
+				hideMap();
+			})
+		}, 3000)
+
+		function hideMap() {
+			vm.showMapState = false;
+		}
 	};
 }
