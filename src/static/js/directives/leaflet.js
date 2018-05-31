0e6131a23dcad9cf06b2f0f579a400e580514a34
Quentin Danneville <quentin@fnfagency.fr>
2018-05-31 21:16:40 +0200

    [Front] Adding top right controls

--- src/static/js/directives/leaflet.js
+++ src/static/js/directives/leaflet.js
@@ -12,21 +12,25 @@ export default function(module) {
 					zoom: 1,
 					crs: L.CRS.Simple,
 					zoomControl: false
-				  });
-				  // dimensions of the image
-				  var w = 4998,
-					  h = 2807,
-					  url = '../static/images/map-background.png';
-
-				  // calculate the edges of the image, in coordinate space
-				  var southWest = map.unproject([0, h], map.getMaxZoom()-1);
-				  var northEast = map.unproject([w, 0], map.getMaxZoom()-1);
-				  var bounds = new L.LatLngBounds(southWest, northEast);
-				  // add the image overlay, 
-				  // so that it covers the entire map
-				  L.imageOverlay(url, bounds).addTo(map);
-				  // tell leaflet that the map is exactly as big as the image
-				  map.setMaxBounds(bounds);
+				});
+				// dimensions of the image
+				var w = 4998,
+				h = 2807,
+				url = '../static/images/map-background.png';
+				
+				// calculate the edges of the image, in coordinate space
+				var southWest = map.unproject([0, h], map.getMaxZoom()-1);
+				var northEast = map.unproject([w, 0], map.getMaxZoom()-1);
+				var bounds = new L.LatLngBounds(southWest, northEast);
+				// add the image overlay, 
+				// so that it covers the entire map
+				L.imageOverlay(url, bounds).addTo(map);
+				// tell leaflet that the map is exactly as big as the image
+				map.setMaxBounds(bounds);
+				
+				L.control.zoom({
+					position:'topright'
+				}).addTo(map);
 			}
 		}
 	}
